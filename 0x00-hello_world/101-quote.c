c8991878445641da1b959a55126431040f81ded9
kondt <mukukakondowe@outlook.com>
2022-12-09 01:10:07 +0200

    Write source code for Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Prints a set of characters to standard error without puts and printf
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
