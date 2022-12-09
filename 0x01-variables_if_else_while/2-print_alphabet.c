012c0939bb36939dcac4016e88ff0b5a5aabd47d
kondt <mukukakondowe@outlook.com>
2022-12-10 01:54:50 +0200

    Write script for Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+  * main - Prints lowercase letters of the alphabet
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int count;
+
+	for (count = 97; count < 123; count++)
+		putchar(count);
+	putchar('\n');
+
+	return (0);
+}
