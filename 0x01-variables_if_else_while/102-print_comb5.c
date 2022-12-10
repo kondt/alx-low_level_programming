a381e04a8ced838a7cac7b2c4bd57a9c5ddec8e3
kondt <mukukakondowe@outlook.com>
2022-12-10 03:48:09 +0200

    Write script for Task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
+
 /**
-  * main - Prints all possible combinations of two digits
+  * main - Prints all possible combinations of two two digits
   *
   * Return: Always 0 (Success)
   */
@@ -9,24 +10,24 @@ int main(void)
 {
 	int c0, c1;
 
-	for (c0 = 48; c0 < 58; c0++)
+	for (c0 = 0; c0 < 98; c0++)
 	{
-		for (c1 = 49; c1 < 58; c1++)
+		for (c1 = 1; c1 <= 99; c1++)
 		{
-			if (c1 > c0)
-			{
-				putchar(c0);
-				putchar(' ');
-				putchar(c1);
-				if (!(c0 == 56 && c1 == 57))
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
+			putchar((c0 / 10) + '0');
+			putchar((c0 % 10) + '0');
+			putchar(' ');
+			putchar((c1 / 10) + '0');
+			putchar((c1 % 10) + '0');
+
+			if (c0 == 98 && c1 == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
 		}
 	}
-	putchar('\n');
 
+	putchar('\n');
 	return (0);
 }
