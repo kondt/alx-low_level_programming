b49270fc272dcf032dfff608e820c882b208ff40
kondt <mukukakondowe@outlook.com>
2022-12-10 02:38:21 +0200

    Write source file for Task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+  * main - Prints all possible combinations of two digits
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int c0, c1;
+
+	for (c0 = 48; c0 < 58; c0++)
+	{
+		for (c1 = 49; c1 < 58; c1++)
+		{
+			if (c1 > c0)
+			{
+				putchar(c0);
+				putchar(c1);
+			}
+			if (c0 < 56 && c1 < 57)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
