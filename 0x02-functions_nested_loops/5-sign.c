60888cb4ea40eab4f9db6025e870e6696caf09de
kondt <mukukakondowe@outlook.com>
2022-12-13 23:35:28 +0200

    Write source for Task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_sign - checks if n is positive, Zero or negative
+ *@n: The integer being tested
+ *
+ * Return: 1 if n is +, 0 if zero and -1 if less that zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+		return (1);
+	else if (n == 0)
+		return (0);
+	else if (n < 0)
+		return (-1);
+}
