ee1d396fcb0d19691f3d2911171dd264b51677f4
kondt <mukukakondowe@outlook.com>
2022-12-14 00:25:26 +0200

    Write source for Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Prints the last digit of a number
+ * @j: The number to obtain the last digit
+ *
+ * Return: The value of the last digit
+ */
+int print_last_digit(int j)
+{
+	return (j % 10);
+}
