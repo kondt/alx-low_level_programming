cfefd9983c47613d7054c5b5c979b04783ffc453
kondt <mukukakondowe@outlook.com>
2022-12-14 01:47:30 +0200

    Write source code for Task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * times_table - Prints the 9 times table
+ *
+ * Return: Nothing
+ */
+void times_table(void)
+{
+	int c0, c1, prod;
+
+	c0 = 0;
+	while (c0 <= 9)
+	{
+		c1 = 0;
+		while (c1 <= 9)
+		{
+			prod = c0 * c1;
+			if (prod < 10)
+			{
+				_putchar(prod);
+			}
+			else
+			{
+				_putchar('0' + (prod / 10));
+				_putchar('0' + (prod % 10));
+			}
+
+			if (c1 != 9)
+			{
+				_putchar(',');
+				_putcahr('\t');
+			}
+			else
+			{
+				_putchar(36);
+				_putchar('\n');
+			}
+			c1++;
+		}
+		c0++;
+	}
+}
