c935513041468ce0abe97196d7971ced27182a33
kondt <mukukakondowe@outlook.com>
2022-12-14 02:43:29 +0200

    Write source for Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers and returns the result
+ * @i: First integer
+ * @j: Second integer
+ *
+ * Return: Sum of the two integers
+ */
+int add(int i, int j)
+{
+	return (i + j);
+}
