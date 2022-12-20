b97c7729c51873f1dd2482c292dff9db00dcde61
kondt <mukukakondowe@outlook.com>
2022-12-20 21:13:03 +0200

    Redo Task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
 /**
-  * main - Prints Number 0 to 9 the newline
-  *
-  * Return: Always 0 (Success)
-  */
+ * main - Prints all single digit base 10 starting from 0, and then a new line
+ * only putchar of stardard library functions is allowed for this program
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	int count;
+	int num;
 
-	for (count = 48; count < 58; count++)
-		putchar(count);
+	num = -1;
+	while (num++ <= 8)
+		putchar(num + '0');
 	putchar('\n');
 
 	return (0);
