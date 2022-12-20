fb3790240c082011f3e70b823a1589b87f9573b8
kondt <mukukakondowe@outlook.com>
2022-12-20 20:01:11 +0200

    Redo Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase except e and q, then a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int count;
+
+	count = 96;
+
+	while (n++ <= 121)
+	{
+		if (n != 101 && n != 113)
+			putchar(count);
+	}
+	return (0);
+}
